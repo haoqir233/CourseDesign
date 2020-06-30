bd39bdf07b53e63b47696c68d6418838db4dd26b
胡启瑞 <757356608@qq.com>
2020-06-30 22:56:18 +0800

    404页面注释增加

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,6 +2,9 @@ import { Button, Result } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/**
+ * 404页面
+ */
 const NoFoundPage = () => (
   <Result
     status="404"
